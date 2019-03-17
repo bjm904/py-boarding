d6f3fc723e002071c52740284d7254636c13e3fb
Bryce Meyer <bjm904@gmail.com>
2019-03-16 21:13:28 -0700

    Many Changes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import random
 import pygame
 import pygame.freetype
 
-NUM_ROWS = 10
-NUM_COLS = 3
-GRID_SIZE = (50, 50) # Width, Height
+NUM_ROWS = 20
+NUM_COLS = 5
+GRID_SIZE = (40, 40) # Width, Height
 SEATS_START_AT = (1, 1) # x, y grid cordinates to start making the seats
-HUMANS_START_AT = (0, 4)
+HUMANS_START_AT = (0, NUM_COLS + 1)
 
 def colToLetter(col):
     if (col == 0):
@@ -21,21 +22,27 @@ def colToLetter(col):
     return 'F'
 
 class Human:
-    def __init__(self, i):
-        self.seatNumber = ()
-        self.x = HUMANS_START_AT[0] - i
+    def __init__(self, row, col):
+        #self.x = HUMANS_START_AT[0] - ((row * NUM_COLS) + col)
+        self.x = HUMANS_START_AT[0] - (row + col * (NUM_ROWS))
         self.y = HUMANS_START_AT[1]
         self.color = (0, 255, 0) # Red, Green, Blue
-        self.size = 15 # Radius
-        self.walkingSpeed = 0.01
+        self.size = round((GRID_SIZE[0] * 0.7) / 2) # Radius
+        #self.walkingSpeed = 0.04 + (random.random() / 10)
+        self.walkingSpeed = 0.05
+        self.seat = Seat(row, col)
+        self.inSeat = False
 
+    def nowInSeat(self):
+        self.inSeat = True
+        self.color = (0, 125, 0)
 
 class Seat:
     def __init__(self, row, col):
         self.row = row # Row relative to this set of seats
         self.col = col # Col relative to this set of seats
         self.color = (0, 0, 255) # Red, Green, Blue
-        self.size = (40, 40) # Width, Height
+        self.size = (GRID_SIZE[0] * 0.8, GRID_SIZE[1] * 0.8) # Width, Height
     
     @property
     def x(self):
@@ -55,17 +62,25 @@ class App:
         self._running = True # Is app running
         self._surface = None # Pygame surface for drawing
         self._font = None # Font used for text
-        self.size = self.weight, self.height = 640, 640 # Size of pygame window
+        screenWidth = (NUM_ROWS + 2) * GRID_SIZE[0]
+        screenHeight = (NUM_COLS + 3) * GRID_SIZE[1]
+        self.size = self.weight, self.height = screenWidth, screenHeight # Size of pygame window
 
         self.seats = [Seat(row, col) for row in range(NUM_ROWS) for col in range(NUM_COLS)] # Create list of all seats, instances of Seat class
-        self.humans = [Human(i) for i in range(NUM_ROWS * NUM_COLS)] # Create human per seat, instances of Human class
+        self.humans = [Human(row, col) for row in range(NUM_ROWS) for col in range(NUM_COLS)] # Create human per seat, instances of Human class
+
+        
 
     def on_init(self):
+        pygame.mixer.pre_init(44100, -16, 2, 2048)
         pygame.init()
         self._surface = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self._font = pygame.freetype.Font('fonts/Roboto-Medium.ttf', 16)
+        self._font = pygame.freetype.Font('fonts/Roboto-Medium.ttf', GRID_SIZE[0] * 0.3)
         pygame.display.set_caption("Py-Boarding")
         self._running = True
+
+        pygame.mixer.init()
+        self.inSeatSound = pygame.mixer.Sound('inSeat.wav')
  
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -73,7 +88,23 @@ class App:
 
     def on_loop(self):
         for human in self.humans:
-            human.x += human.walkingSpeed
+            if (human.inSeat == False):
+                diffToSeatX = (human.seat.row + SEATS_START_AT[0]) - human.x
+                diffToSeatY = (human.seat.col + SEATS_START_AT[1]) - human.y
+                if (diffToSeatX != 0):
+                    if (diffToSeatX > 0):
+                        human.x += min(human.walkingSpeed, diffToSeatX)
+                    else :
+                        human.x += max(-human.walkingSpeed, diffToSeatX)
+                elif (diffToSeatY != 0):
+                    if (diffToSeatY > 0):
+                        human.y += min(human.walkingSpeed, diffToSeatY)
+                    else :
+                        human.y += max(-human.walkingSpeed, diffToSeatY)
+                else:
+                    human.nowInSeat()
+                    self.inSeatSound.play()
+            
 
     def on_render(self):
         self._surface.fill((0,0,0)) # Fill black backgounrd, also for clearing
@@ -86,12 +117,13 @@ class App:
             centeringOffsetY = (GRID_SIZE[1] - seat.size[1]) / 2
             rectX = gridCellX + centeringOffsetX
             rectY = gridCellY + centeringOffsetY
-            pygame.draw.rect(self._surface, seat.color, [rectX, rectY, seat.size[0], seat.size[1]], 2)
+            pygame.draw.rect(self._surface, seat.color, [rectX, rectY, seat.size[0], seat.size[1]], 3)
 
             # Draw seat number near center of seat
-            textX = gridCellX + (GRID_SIZE[0] / 2)
-            textY = gridCellY + (GRID_SIZE[1] / 2)
-            self._font.render_to(self._surface, (textX, textY), seat.seatNumber, (255, 255, 255))
+            textSurface, textRect = self._font.render(seat.seatNumber, (255, 255, 255))
+            textX = gridCellX + (GRID_SIZE[0] / 2) - (textRect.width / 2)
+            textY = gridCellY + (GRID_SIZE[1] / 2) - (textRect.height / 2)
+            self._surface.blit(textSurface, (textX, textY))
 
         for human in self.humans:
             gridCellX = GRID_SIZE[0] * human.x
@@ -103,9 +135,10 @@ class App:
             pygame.draw.circle(self._surface, human.color, (rectX, rectY), int(human.size))
 
             # Draw human seatNumber near center of human
-            #textX = gridCellX + (GRID_SIZE[0] / 2)
-            #textY = gridCellY + (GRID_SIZE[1] / 2)
-            #self._font.render_to(self._surface, (textX, textY), str(human.seatNumber), (255, 255, 255))
+            textSurface, textRect = self._font.render(human.seat.seatNumber, (0, 0, 0))
+            textX = rectX - (textRect.width / 2)
+            textY = rectY - (textRect.height / 2)
+            self._surface.blit(textSurface, (textX, textY))
 
         pygame.display.flip() # Render whole screen
 
